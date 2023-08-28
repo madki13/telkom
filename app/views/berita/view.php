452cf20f62de9aa78ae1ecb20d41397eedc80574
CaptoreZ <lateefmin11@gmail.com>
2023-08-28 11:13:03 +0700

    test 2

--- app/views/berita/view.php
+++ app/views/berita/view.php
@@ -1,33 +1,30 @@
 <?php
-
 use yii\helpers\Html;
-use yii\widgets\DetailView;
-use yii\widgets\ListView;
+use yii\web\View;
+use yii\web\YiiAsset;
 use yii\data\ActiveDataProvider;
 
-
-
-/** @var yii\web\View $this */
+/** @var View $this */
 /** @var app\models\Berita $model */
 
+$photo = $model->photo;
+
 $this->title = $model->judul;
-
-
-\yii\web\YiiAsset::register($this);
+YiiAsset::register($this);
 ?>
 
 <div id="berita-view">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="container">
+    <div class="container mt-5">
         <div class="row">
-            <div class="col-lg-12 col-md-12">
-                <div class="berita-view">
-                    <div class="berita-text">
-                        <h3><?= $model->judul ?></h3>
-                        <span class="posted_on"><?= $model->tanggal_terbit ?></span>
-                        <div class="content">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-img-container">
+                        <img src="<?= Yii::getAlias('@web') . '/' . $model->photo ?>" class="card-img" alt="<?= Html::encode($model->judul) ?>">
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title"><?= Html::encode($model->judul) ?></h1>
+                        <p class="card-text posted_on"><?= $model->tanggal_terbit ?></p>
+                        <div class="card-text content">
                             <?= $model->konten ?>
                         </div>
                     </div>
@@ -36,3 +33,18 @@ $this->title = $model->judul;
         </div>
     </div>
 </div>
+
+<style>
+    .card-img-container {
+        width: 100%; /* Sesuaikan lebar container gambar */
+        height: 250px; /* Sesuaikan tinggi container gambar */
+        overflow: hidden;
+        margin-bottom: 10px; /* Sesuaikan margin bawah */
+    }
+
+    .card-img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover; /* Sesuaikan object-fit sesuai kebutuhan (cover/contain/fill) */
+    }
+</style>
