aeeddced3a98dfb270e1547f9170c29e5cb6a4d5
CaptoreZ <lateefmin11@gmail.com>
2023-08-29 14:15:14 +0700

    view berita

--- app/views/berita/view.php
+++ app/views/berita/view.php
@@ -1,4 +1,5 @@
 <?php
+
 use yii\helpers\Html;
 use yii\web\View;
 use yii\web\YiiAsset;
@@ -16,13 +17,21 @@ YiiAsset::register($this);
 <div id="berita-view">
     <div class="container mt-5">
         <div class="row">
-            <div class="col-lg-12 col-md-12">
-                <div class="berita-view">
-                    <div class="berita-text">
-                        <h3><?= $model->judul ?></h3>
-                        <span class="posted_on"><?= $model->tanggal_terbit ?></span>
-                        <div class="content">
-                            <?= $model->konten ?>
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-img-container">
+                        <img src="<?= Yii::getAlias('@web') . '/' . $model->photo ?>" class="card-img" alt="<?= Html::encode($model->judul) ?>">
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title display-4"><?= Html::encode($model->judul) ?></h1>
+                        <p class="card-text text-muted"><?= $model->tanggal_terbit ?></p>
+                        <div class="card-text lead">
+                            <?php
+                                $paragraphs = explode("\n", $model->konten);
+                                foreach ($paragraphs as $paragraph) {
+                                    echo "<p>" . Html::encode($paragraph) . "</p>";
+                                }
+                            ?>
                         </div>
                     </div>
                 </div>
@@ -33,15 +42,20 @@ YiiAsset::register($this);
 
 <style>
     .card-img-container {
-        width: 100%; /* Sesuaikan lebar container gambar */
-        height: 250px; /* Sesuaikan tinggi container gambar */
+        width: 100%;
+        /* Sesuaikan lebar container gambar */
+        height: 300px;
+        /* Sesuaikan tinggi container gambar */
         overflow: hidden;
-        margin-bottom: 10px; /* Sesuaikan margin bawah */
+        margin-bottom: 10px;
+        /* Sesuaikan margin bawah */
     }
 
     .card-img {
         width: 100%;
         height: 100%;
-        object-fit: cover; /* Sesuaikan object-fit sesuai kebutuhan (cover/contain/fill) */
+        object-fit: cover;
+        /* Sesuaikan object-fit sesuai kebutuhan (cover/contain/fill) */
     }
+
 </style>
