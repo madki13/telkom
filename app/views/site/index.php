23164bf640a13f755caf9f63f7796e2409e6fd5e
Your Name <raaaaygbs@gmail.com>
2023-09-25 21:09:47 +0700

    memperbagus halaman home

--- app/views/site/index.php
+++ app/views/site/index.php
@@ -24,7 +24,7 @@ $this->title = 'My Yii Application';
     .wrapper {
       background: url(https://digitalbisa.id/56tahun/img/sejarah-bawah-gedung.png );
       background-size: cover;
-      height: 110vh;
+      height: 100vh;
       margin: -70px;
       display: flex;
       align-items: center;
@@ -44,56 +44,64 @@ $this->title = 'My Yii Application';
     }
 
     .center h1 {
-      font-size: 55px;
-      width: 900px;
+      display: block;
       text-align: center;
+      font-size: 55px;
       font-family: 'Poppins', sans-serif;
-      margin-left: 20px;
       font-weight: 800;
-
-
     }
 
     .center h2 {
-      font-size: 50px;
-      width: 885px;
-      margin-top: 10px;
+      display: block;
       text-align: center;
+      font-size: 50px;
       font-family: 'Poppins', sans-serif;
-      margin-left: 22px;
-      font-weight: 800;
+      font-weight: 600;
     }
 
     .center h5 {
+      display: block;
+      text-align: center;
       font-size: 25px;
-      font-weight: 800;
-      margin-left: 280px;
       font-family: 'Poppins', sans-serif;
+      font-weight: 600;
     }
 
     .bagian {
-      padding-top: 100Px;
-      font-size: 30px;
-      margin-left: 470px;
+      padding-top: 150px;
+      display: block;
+      text-align: center;
+    }
 
+    .text-bagian {
+      font-size: 36px;
+      font-weight: 700;
     }
 
     .isi {
       display: flex;
-      margin-top: 5%;
+      margin-top: 4%;
       justify-content: space-between;
       padding: 60px;
+      
 
     }
 
     .content {
+      color: white;
       flex-basis: 31%;
-      background-color: #FF6A61;
+      background-color: #c91717;
       border-radius: 10px;
-      padding: 20px 10px;
+      padding: 30px 20px;
       box-sizing: border-box;
       text-align: center;
       font-family: 'Poppins', sans-serif;
+      box-shadow: 0px 0px 96px -11px rgba(0,0,0,0.56);
+    }
+
+    .judul-content {
+      font-weight: 600;
+      font-size: 27px;
     }
     .struktur{
         align-items: center;
@@ -107,7 +115,7 @@ $this->title = 'My Yii Application';
     }
     .struktur h6{
       margin: 0;
-      margin-top: -150px;
+      margin-top: -110px;
       font-family: 'Poppins', sans-serif;
       font-weight: bold;
       font-size: 40px;
@@ -139,25 +147,29 @@ $this->title = 'My Yii Application';
     .pound img{
       width: 100%;
       border-radius: 10px;
+      box-shadow: 10px 18px 38px -3px rgba(153,153,153,0.98);
     }
 
-    
+    .judul-pound {
+      margin-top: 20px;
 
+    }
+
+  
     .utopia h7{
+      display: block;
+      text-align: center;
       font-size: 40px;
-      margin-top: 5px;
-      margin-left: 380px;
       font-family: 'Poppins', sans-serif;
-      padding: 100px;
+
     }
     .utopia p2{
+      display: block;
+      text-align: center;
       font-size: 20px;
-      margin-top: 100px;
-      margin-left: 480px;
       font-family: 'Poppins', sans-serif;
-
-
     }
+    
     .profile{
       width: 80%;
       margin: auto;
@@ -194,16 +206,26 @@ $this->title = 'My Yii Application';
       margin-top: -15%;
       margin-left: -15%
     }
+
+
+    /* .block-merah {
+      width: 100%;
+      height: 10vh;
+      background-color: #c91717;
+      margin-bottom: 30px;
+    } */
+
     * {
 	padding: 0px;
 	margin: 0px;
 	box-sizing: border-box;
 }
 .struktur-perusahaan {
-	height: 100vh;
+	height: 60vh;
 	display: grid;
 	align-items: center;
 	font-family: 'Poppins', sans-serif;
+  margin-bottom: 100px;
 }
 .tree {
 	width: 100%;
@@ -312,53 +334,55 @@ $this->title = 'My Yii Application';
     <h5> WELCOME TO OUR WEBSITE</h5>
   </div>
   <div class="bagian">
-    <h3>
+    <h3 class="text-bagian">
       SEPUTAR TELKOM INDONESIA
     </h3>
   </div>
   <div class="isi">
     <div class="content">
-      <h4>TELKOM</h4>
+      <h4 class="judul-content">TELKOM</h4>
       <p> Telkom Indonesia adalah penyedia layanan telekomunikasi yang menyediakan berbagai layanan seperti telepon tetap, telepon seluler, internet, dan layanan data.</p>
     </div>
     <div class="content">
-      <h4>SEJARAH</h4>
+      <h4 class="judul-content">SEJARAH</h4>
       <p>Telkom Indonesia didirikan pada tahun 1856 dan sejak itu telah berkembang menjadi perusahaan terkemuka di industri telekomunikasi Indonesia. Perusahaan ini memiliki misi untuk menyediakan layanan telekomunikasi berkualitas tinggi kepada pelanggan di seluruh Indonesia.</p>
     </div>
     <div class="content">
-      <h4>VISI</h4>
+      <h4 class="judul-content">VISI</h4>
       <p> Dengan visi untuk menjadi Digital Telecommunication Company of the Region, Telkom Indonesia terus berinovasi dan berinvestasi dalam teknologi terbaru untuk memenuhi kebutuhan pelanggan di era digital.</p>
     </div>
   </div>
 
   <div class="struktur">
     <img src="https://www.telkom.co.id/data/image_upload/page/1594112895830_compress_PNG%20Icon%20Telkom.png" alt="">
-    <h6> Apa itu IS dan IPN?</h6>
-    <p1> IS adalah bagian dari TELKOM STO GAMBIR yaitu menangani instalsai infrasturuktur jaringan jaringan network telekomunikasi dan seatmanajemen laptop dekstop printer dan infokus digedung TELKOM. IPN mentenes metro untuk witel jakarta pusat </p1>
+    <h6> Apa itu IS dan IPN dan Swtiching?</h6>
+    <p1> </p1>
     
   </div>
   
   <div class="isi">
     <div class="pound">
       <img src="https://ik.trn.asia/uploads/2021/08/1627894603712.jpeg" alt="">
-      <h4>COFFE MORNING</h4>
-      <p> Telkom Indonesia adalah penyedia layanan telekomunikasi yang menyediakan berbagai layanan seperti telepon tetap, telepon seluler, internet, dan layanan data.</p>
+      <h4 class="judul-pound">IS</h4>
+      <p> IS, unit kerja di bawah TELKOM STO Gambir, bertanggung jawab atas instalasi dan pengelolaan infrastruktur jaringan, jaringan telekomunikasi, dan seat management perangkat elektronik (laptop, desktop, printer, dan infocus) di seluruh gedung TELKOM.</p>
     </div>
     <div class="pound">
       <img src="https://digitaltransformation.co.id/wp-content/uploads/2023/03/young-man-engineer-making-program-analyses-1-1.jpg" alt="">
-      <h4>NETWORKING</h4>
-      <p>Telkom Indonesia didirikan pada tahun 1856 dan sejak itu telah berkembang menjadi perusahaan terkemuka di industri telekomunikasi Indonesia. Perusahaan ini memiliki misi untuk menyedi.</p>
+      <h4 class="judul-pound">IPN</h4>
+      <p>IPN, unit kerja di bawah TELKOM STO Gambir, bertanggung jawab atas pemeliharaan jaringan metro di seluruh wilayah kerja Witel Jakarta Pusat.     </p>
     </div>
     <div class="pound">
       <img src="https://plus.unsplash.com/premium_photo-1683134221762-25385482e8ac?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxMjA3fDB8MXxzZWFyY2h8NDB8fGNhYmxlJTIwbGluZXxlbnwwfHx8fDE2ODgxNDU5NjJ8MQ&ixlib=rb-4.0.3&q=80&w=1080" alt="">
-      <h4>VISI</h4>
-      <p> Dengan visi untuk menjadi Digital Telecommunication Company of the Region, Telkom Indonesia terus berinovasi dan berinvestasi dalam teknon</p>
+      <h4 class="judul-pound">Switching</h4>
+      <p>Proses menghubungkan satu atau lebih perangkat untuk melakukan komunikasi. Perangkat tersebut dapat berupa terminal, seperti telepon, komputer, atau perangkat lainnya. </p>
     </div>
   </div>
 
+  <div class="block-merah"></div>
+
   <div class="utopia">
-    <h7> STRUKTUR BIDANG IS</h7>
-    <p2> inilah beberapa struktur bagian is di telkom gambir</p2>
+    <h7> STRUKTUR BIDANG IS, IPN, Switching</h7>
+    <p2>Struktur bagian IS, IPN, Switching di Telkom Gambir</p2>
   </div>
 
  
@@ -367,21 +391,22 @@ $this->title = 'My Yii Application';
 		<div class="filipo">
 			<div class="pipo">
 				<div class="tree">
-					<ul>
+        <ul>
 						<li> <a href="#"><span>GM WITEL JAKPUS</span></a>
-						<ul>
-						<li> <a href="#"><img src="images/5.jpg"><span>Necromancer</span></a>
-						<ul>
-							<li> <a href="#"><img src="images/1.jpg"><span>Beliau Pertama</span></a> </li>
-							<li> <a href="#"><img src="images/2.jpg"><span>Beliau Kedua</span></a> </li>
-							<li> <a href="#"><img src="images/3.jpg"><span>Beliau Ketiga</span></a> </li>
-							<li> <a href="#"><img src="images/4.jpg"><span>Beliau Keempat</span></a> </li>
-							
-						</ul>
+					<ul>
+					<ul>
+          <li> <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/4792/4792929.png"><span>Ahmad Mulyono</span></a>
+					<ul>
+						<li> <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/4792/4792929.png"><span>Riyanto</span></a>
+					<ul>
+							<li> <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/180/180658.png"><span>Beliau Pertama</span></a> </li>
+							<li> <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/180/180658.png"><span>Beliau Kedua</span></a> </li>
+							<li> <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/6833/6833591.png"><span>Beliau Ketiga</span></a> </li>
+							<li> <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/180/180658.png"><span>Beliau Keempat</span></a> </li>
+					</ul>
 					</li>
 
-					
-
+				
 				</ul>
 			</li>
 		</ul>
